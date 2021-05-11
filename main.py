b324e18092aed3b2cf70b22768f8262a24ee155d
serg <sergeyrs@bk.ru>
2021-05-11 15:00:06 +0300

    First commit

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Hello! This is the main page"
+    return "Hello! This is the main page <h1>HELLO</h1>"
 
 
 @app.route("/<name>")
